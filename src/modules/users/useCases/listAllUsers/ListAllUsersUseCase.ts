54cf7d11ec7c7fd6cecde9e50850dcb7b7288a8a
Gabriel Henrique <gabrielhenrique_3l@outlook.com>
2021-03-24 22:07:55 -0300

    teste concluido

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,12 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userExists = this.usersRepository.findById(user_id);
+
+    if (!userExists || !userExists.admin)
+      throw new Error("User is not admin or not exists");
+
+    return this.usersRepository.list();
   }
 }
 
